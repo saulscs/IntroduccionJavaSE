0cc7705eea95e684574c3e42f40892de8d5731f0
Saul Corona <saulcoronasantos@ymail.com>
2021-04-15 22:40:27 -0500

    Add NamingJava

---
+++ src/NamingJava.java
@@ -0,0 +1,26 @@
+//Upper Camel Case  - Siempre van asi las clases
+public class NamingJava {
+    public static void main(String[] args){
+        int celphone = 123123;
+        int celPhone = 23213;
+        System.out.println(celPhone);
+        System.out.println(celphone);
+
+        String $contryName = "Spain";
+        String _backgrounColor = "RED";
+
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -4;
+        int MAX_WIDTH = 999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+
+        String fullName = "Anahi";
+        int sizeInCentimeters = 26;
+
+
+    }
+}
